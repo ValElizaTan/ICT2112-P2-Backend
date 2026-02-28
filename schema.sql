074ae45ed698563844d055382f311caef0288e53
Kai Wen <taykaiwen1111@gmail.com>
2026-02-28 22:48:39 +0800

    Team 5 (table 1)

--- schema.sql
+++ schema.sql
@@ -1,1 +1,10 @@
-
+--TEAM 5 001_building_footprint table
+CREATE TABLE IF NOT EXISTS BuildingFootprint (
+  buildingCarbonFootprintID SERIAL PRIMARY KEY,
+  timeHourly TIMESTAMP NOT NULL,
+  zone VARCHAR(50),
+  block VARCHAR(50),
+  floor VARCHAR(50),
+  room VARCHAR(50),
+  totalRoomCo2 DOUBLE PRECISION NOT NULL
+);
